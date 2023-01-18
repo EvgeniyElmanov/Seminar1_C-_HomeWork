360a93dba942ba2bb7cc338c8bd4e70d44ab9896
Elmanov_Evgeniy <elmanov21@gmail.com>
2023-01-18 10:03:53 +0800

    Add task_10

---
+++ HomeWork_2/task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа 
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int numberA = number / 10;
+int numberB = numberA % 10;
+
+Console.WriteLine(numberB);
