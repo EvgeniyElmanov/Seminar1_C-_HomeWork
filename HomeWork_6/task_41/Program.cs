a2ab500c4846024e12d84073484bb5e2b3694ebf
Elmanov_Evgeniy <elmanov21@gmail.com>
2023-02-03 00:24:54 +0800

    Add Task_41_43

---
+++ HomeWork_6/task_41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int CountNumber(int[] num)
+{
+  int count = 0;
+  for (int i = 0; i < num.Length; i++)
+  {
+    if (num[i] > 0) count++;
+  }
+  return count;
+}
+
+int[] GetNumber(int size)
+{
+  int[] arr = new int[size];
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{i + 1}: ");
+    arr[i] = int.Parse(Console.ReadLine());
+  }
+  return arr;
+}
+
+int EntNumber(string text)
+{
+  Console.WriteLine(text);
+  int number = int.Parse(Console.ReadLine());
+  return number;
+}
+
+int number = EntNumber("Enter number: ");
+int[] res = GetNumber(number);
+Console.WriteLine(string.Join(",", res) + " -> " + CountNumber(res));
---
+++ HomeWork_6/task_43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void InrerPoint(double b1, double k1, double b2, double k2)
+{
+  double x = (b2 - b1) / (k1 - k2);
+  double y = k1 * x + b1; 
+  Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
+}
+
+double GetNum(string text)
+{
+  Console.Write(text);
+  double number = double.Parse(Console.ReadLine());
+  return number;
+}
+
+double b1 = GetNum("Enter point  B1: ");
+double k1 = GetNum("Enter point  K1: ");
+double b2 = GetNum("Enter point  B2: ");
+double k2 = GetNum("Enter point  K2: ");
+
+InrerPoint(b1, k1, b2, k2);
